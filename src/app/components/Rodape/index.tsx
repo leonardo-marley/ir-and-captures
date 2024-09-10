bb50897a88b3b29ad24c45f087de2f8d1799be0e
Yure Luis <yurerj@hotmail.com>
2024-09-10 11:14:17 -0300

    UPD - RODAPE E SOBRE

--- src/app/components/Rodape/index.tsx
+++ src/app/components/Rodape/index.tsx
@@ -21,37 +21,38 @@ export default function Rodape() {
       { isClient &&
         <footer className={styles.rodape}>
           <div className={styles.container}>
+            <div className={styles.colunaContato}>
+                <nav>
+                  <p className={styles.contato} >Home</p>
+                </nav>
+                <nav>
+                  <p className={styles.contato}>Downloads</p>
+                </nav>
+                <nav>
+                  <p className={styles.contato}>Contato</p>
+                </nav>
+                <nav>
+                  <p className={styles.contato}>Sobre</p>
+                </nav>
+              </div>
             <div className={styles.coluna}>
-              <h2>Redes Sociais</h2>
-              <ul>
-                <li>
-                  <a className={styles.redes} href="#">< FacebookIcon/> Facebook
-                  </a>
-                </li>
-                <li>
-                  <a className={styles.redes} href="#">< InstagramIcon /> Instagram
-                  </a>
-                </li>
-                <li>
-                  <a className={styles.redes} href="#">< XIcon /> Twitter
-                  </a>
-                </li>
-              </ul>
+              {/* <h2>Redes Sociais</h2> */}
+                <span>
+                  <p className={styles.redes} >< FacebookIcon/> 
+                  </p>
+                </span>
+                <span>
+                  <p className={styles.redes} >< InstagramIcon /> 
+                  </p>
+                </span>
+                <span>
+                  <p className={styles.redes} >< XIcon /> 
+                  </p>
+                </span>
             </div>
-            <div className={styles.coluna}>
-              <h2>Contato ou Informações</h2>
-              <ul className={styles.listaContato}>
-                <li>
-                  <a className={styles.contato} href="#">< MailOutlineIcon /> email@email.com</a>
-                </li>
-                <li>
-                  <a className={styles.contato}>< LocalPhoneIcon />(21) 4002-8922</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className={styles.direitos}>
-            <p>IR and Captures &copy; 2024</p>
+              <div className={styles.direitos}>
+                <p>Copyright &copy; 2024 IR and Captures</p>
+              </div>
           </div>
         </footer>
       }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -573,7 +573,7 @@ export default function Home() {
                         display: 'flex',
                         alignSelf: 'flex-end',
                         '& .Mui-selected': {
-                          backgroundColor: '#9d2053 !important',
+                          backgroundColor: '',
                           color: '#fff',
                           '&:hover': {
                             backgroundColor: '#9d205380', // Cor opaca no hover
@@ -600,8 +600,11 @@ export default function Home() {
         );
       case 4:
         return (
-          <div className={styles.containerConteudo}>
-            <h1>Sobre</h1>
+          <div className={styles.containerSobre}>
+            <div className={styles.containerTexto}>
+                <h1>Sobre o IRs and Captures</h1>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Este projeto foi criado para músicos e entusiastas que buscam explorar e compartilhar arquivos de Impulse Responses (IRs) e capturas para pedais de guitarra. Nossa plataforma permite que os usuários façam upload e download de arquivos, oferecendo um espaço colaborativo para a criação de timbres únicos. Além disso, a comunidade integrada facilita a comunicação entre os membros, possibilitando discussões, dicas e o intercâmbio de experiências. Aqui, você encontra tudo o que precisa para expandir suas possibilidades sonoras e conectar-se com outros músicos apaixonados.</p>
+            </div>
           </div>
         );
       case 5:
